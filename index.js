3d694509035661fe54e9aca566ba92758a68e5d6
LAPTOP-TJPTOSHR\moksh <mokshith@shazab.in>
2024-06-05 23:51:31 +0530

    Navbar

---
+++ index.js
@@ -0,0 +1,11 @@
+//send message
+let username;
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+}
+
+// function click(){
+//     username=document.getElementById("mytext").value;
+//     console.log(username);
+// }
